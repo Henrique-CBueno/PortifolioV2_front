60c70fbfbe05d28d04fa12ed05816a3d90ddf3e1
Henrique-CBueno <henriquecb2507@outlook.pt>
2026-03-14 16:16:04 -0300

    ADD FOOTER SECTION

--- front_portifolioV2/src/App.tsx
+++ front_portifolioV2/src/App.tsx
@@ -1,5 +1,6 @@
 import About from "./components/About/About"
 import Contact from "./components/Contact/Contact"
+import Footer from "./components/Footer/Footer"
 import Header from "./components/Header/Header"
 import Journey from "./components/Journey/Journey"
 import Main from "./components/Main/Main"
@@ -19,6 +20,7 @@ function App() {
         <Stacks />
         <Projects />
         <Contact />
+        <Footer />
     </div>
   )
 }
